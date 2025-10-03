a7901c47734b55e5c749866963f86b307d430ab0
Oleksii-Ivan Stetsyk <farshaleks@gmail.com>
2025-10-03 18:07:53 +0300

    add tests for checkPassword function

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 'use strict';
 
 describe(`Function 'checkPassword':`, () => {
@@ -8,12 +9,34 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof checkPassword('12345678')).toEqual('boolean');
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
-
+    expect(checkPassword('P@ssw0rd!')).toEqual(true);
   });
 
-  // write more tests here
+  it(`should return 'false' when the password is less than 8 characters`, () => {
+    expect(checkPassword('P@sw0rd')).toEqual(false);
+  });
+
+  it(`should return 'false' when the password is more than 16 characters`, () => {
+    expect(checkPassword('P@ssw0rd!P@ssw0rd!')).toEqual(false);
+  });
+
+  it(`should return 'false' when password without big letter`, () => {
+    expect(checkPassword('p@ssw0rd!')).toEqual(false);
+  });
+
+  it(`should return 'false' when password without small letter`, () => {
+    expect(checkPassword('P@SSW0RD!')).toEqual(false);
+  });
+
+  it(`should return 'false' when password without special character`, () => {
+    expect(checkPassword('Passw0rd')).toEqual(false);
+  });
+
+  it(`should return 'false' when password with spaces`, () => {
+    expect(checkPassword('P@s sw0rd!')).toEqual(false);
+  });
 });
