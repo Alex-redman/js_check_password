fa8b224dc488a1f8baf7a6fa958eae32151cb256
Oleksii-Ivan Stetsyk <farshaleks@gmail.com>
2025-10-03 18:29:59 +0300

    update tests

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -12,31 +12,75 @@ describe(`Function 'checkPassword':`, () => {
     expect(typeof checkPassword('12345678')).toEqual('boolean');
   });
 
-  it(`should return 'true' for the valid password with 8 characters`, () => {
+  it(`should return true for valid password with exactly 8 characters`, () => {
+    expect(checkPassword('P@ssw0r1')).toEqual(true);
+  });
+
+  it(`should return true for valid password with 9 characters`, () => {
     expect(checkPassword('P@ssw0rd!')).toEqual(true);
   });
 
-  it(`should return 'false' when the password is less than 8 characters`, () => {
+  it(`should return false when the password is less than 8 characters`, () => {
     expect(checkPassword('P@sw0rd')).toEqual(false);
   });
 
-  it(`should return 'false' when the password is more than 16 characters`, () => {
+  it(`should return true for valid password with exactly 16 characters`, () => {
+    expect(checkPassword('P@ssw0rd!P@ssw0r')).toEqual(true);
+  });
+
+  it(`should return false when the password is more than 16 characters`, () => {
     expect(checkPassword('P@ssw0rd!P@ssw0rd!')).toEqual(false);
   });
 
-  it(`should return 'false' when password without big letter`, () => {
+  it(`should return false when password has no uppercase letter`, () => {
     expect(checkPassword('p@ssw0rd!')).toEqual(false);
   });
 
-  it(`should return 'false' when password without small letter`, () => {
+  it(`should return false when password has no lowercase letter (function requires both upper and lower)`, () => {
     expect(checkPassword('P@SSW0RD!')).toEqual(false);
   });
 
-  it(`should return 'false' when password without special character`, () => {
+  it(`should return false when password has no digit`, () => {
+    expect(checkPassword('Password!')).toEqual(false);
+  });
+
+  it(`should return false when password has no special character`, () => {
     expect(checkPassword('Passw0rd')).toEqual(false);
   });
 
-  it(`should return 'false' when password with spaces`, () => {
+  it(`should return false when password contains spaces`, () => {
     expect(checkPassword('P@s sw0rd!')).toEqual(false);
+  });
+
+  it(`should return true for example password 'Password1!'`, () => {
+    expect(checkPassword('Password1!')).toEqual(true);
+  });
+
+  it(`should return false for example password 'qwerty'`, () => {
+    expect(checkPassword('qwerty')).toEqual(false);
+  });
+
+  it(`should return false for example password 'Str@ng'`, () => {
+    expect(checkPassword('Str@ng')).toEqual(false);
+  });
+
+  it(`should return true when password contains non-Latin letters like ä (function does not block them)`, () => {
+    expect(checkPassword('Pässw0rd!')).toEqual(true);
+  });
+
+  it(`should return false when password contains Cyrillic letters (Рassword1!)`, () => {
+    expect(checkPassword('Рassword1!')).toEqual(false);
+  });
+
+  it(`should return true for minimal valid password with all required categories (A1!aaaaa)`, () => {
+    expect(checkPassword('A1!aaaaa')).toEqual(true);
+  });
+
+  it(`should return true for password with multiple specials and digits (P@ssw0rd!!11)`, () => {
+    expect(checkPassword('P@ssw0rd!!11')).toEqual(true);
+  });
+
+  it(`should return false when password has uppercase + special but no digit (Passw@rd)`, () => {
+    expect(checkPassword('Passw@rd')).toEqual(false);
   });
 });
